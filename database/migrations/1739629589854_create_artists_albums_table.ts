831cbdc3561d5527ad4d857b5182bf92a0342971
mmasser95 <mmasser95@gmail.com>
2025-02-15 15:55:29 +0100

    TASK: Refactor album and song migrations to comment out foreign key constraints and add new artists_albums pivot table

--- database/migrations/1739629589854_create_artists_albums_table.ts
+++ database/migrations/1739629589854_create_artists_albums_table.ts
@@ -1,16 +1,13 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'albums'
+  protected tableName = 'album_artists'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('name').notNullable()
-      table.string('cover_image').nullable()
-      table.text('description').nullable()
-      // table.integer('artist_id').unsigned().references('id').inTable('artists').onDelete('CASCADE')
-      table.string("spotify_id").nullable()
+      table.integer('album_id').unsigned().references('id').inTable('albums').onDelete('CASCADE')
+      table.integer('artist_id').unsigned().references('id').inTable('artists').onDelete('CASCADE')
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
