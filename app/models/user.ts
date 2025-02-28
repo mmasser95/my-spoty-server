f922cfcc6bced345cb47c5e710329ec07f727c80
mmasser95 <mmasser95@gmail.com>
2025-02-28 11:57:09 +0100

    TASK: Remove token expiration time from access token configuration

--- app/models/user.ts
+++ app/models/user.ts
@@ -41,7 +41,6 @@ export default class User extends compose(BaseModel, AuthFinder) {
   declare updatedAt: DateTime | null
 
   static accessTokens = DbAccessTokensProvider.forModel(User, {
-    expiresIn: '7 days',
     prefix: 'oat_',
     table: 'auth_access_tokens',
     type: 'auth_token',
